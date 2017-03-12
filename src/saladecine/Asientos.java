e4ac01f95b8e126035f4b00b478315fdafc4984c
Jonathan <Jonathan@Jonathan-PC>
2017-03-12 17:23:06 -0500

    se crea la clase main, pelicula que contiene los atributos y detalles de la pelicula y asientos que contiene la cantidad de filas y columnas deseadas

---
+++ src/saladecine/Asientos.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package saladecine;
+
+/**
+ *
+ * @author Jonathan
+ */
+public class Asientos {
+    /**
+     * atributo que contiene la cantidad de filas
+     */
+    private int filas;
+    /**
+     * attributo que contiene la cantidad de columnas
+     */
+    private int columnas;
+    public Asientos() {
+        
+    }//constructor
+    
+    public int pregunta(){
+        
+        return ;
+    }
+}//Asientos
